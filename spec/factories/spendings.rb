34075a616664f61d1754b46d25e0535a8fba6c08
tim <tima.muravyov@gmail.com>
2023-02-01 00:47:30 +0200

    fix(factories): add name field to spending

--- spec/factories/spendings.rb
+++ spec/factories/spendings.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :spending do
+    name { Faker::Lorem.word }
     description { Faker::Lorem.sentence }
     amount { Faker::Number.decimal(l_digits: 2) }
     category { Spending.categories.keys.sample }
