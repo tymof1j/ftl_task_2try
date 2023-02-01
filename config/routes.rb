96c1f9ab3a189327aed4f8171d34016588f89b0b
tim <tima.muravyov@gmail.com>
2023-02-01 14:46:59 +0200

    feat(routes): add for shared_lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :spendings
+  resources :shared_lists, except: %i[destroy update]
   root to: 'spendings#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
